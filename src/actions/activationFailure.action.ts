6b80cb9cd86344b91bd1c65bf088c3900cb4e6e5
Ari Bredall <33331177+abredall@users.noreply.github.com>
2024-02-01 07:49:25 +0000

    rm gif

--- src/actions/activationFailure.action.ts
+++ src/actions/activationFailure.action.ts
@@ -5,7 +5,6 @@ import { leaderboards } from '../altbot';
 export async function activationFailure(originalMessage: Message<boolean>) {
   // const expireTime = 30;
   if (!leaderboards.UserSettings?.[originalMessage.author.id]?.ActivationFailure) return;
-  await originalMessage.reply("https://imgur.com/NTw02QA");
     // .then(theReply => {
     //   setTimeout(() => theReply.delete(), expireTime * 1000);
     // });
